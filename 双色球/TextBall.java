c1542c8e836d2dcb19eb818839e6d336524058d0
SunWolfer <1308487197@qq.com>
2018-01-11 10:14:45 +0800

    123456

--- 双色球/TextBall.java
+++ 双色球/TextBall.java
@@ -11,36 +11,37 @@ public class TextBall{
 		int count = 1;
 
 
-        //æŒ‘é€‰çº¢çƒ
+        //Ëæ»ú³éºìÇò
         while(true){
         	for(int i=0;i<6;i++) {
-        		int[] rednum = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33};//çº¢çƒåˆå§‹å€¼
+        		int[] rednum = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33};
            		aa:
            		while(true) {
-              	 		 int red = ball1.nextInt(33);
-              	 		 if(rednum[red] != 0) {
-               	    			 test[i] = rednum[red];
-               	    			 rednum[red] = 0;
-               	    			 break aa;
-               	 		}
+              	  int red = ball1.nextInt(33);
+              	  if(rednum[red] != 0) {
+               	     test[i] = rednum[red];
+               	     rednum[red] = 0;
+               	     break aa;
+               	 	}
 
            	 	}
         	}
 
-           	int lucky = ball2.nextInt(16)+1;//è“è‰²çƒ
+           	int lucky = ball2.nextInt(16)+1;
         	Arrays.sort(test);
-		Arrays.sort(win);
-		String test1 = Arrays.toString(test);
-		String win1 = Arrays.toString(win);
+			Arrays.sort(win);
+			/*String test1 = Arrays.toString(test);
+			String win1 = Arrays.toString(win);*/
 
 
-		if(test1.equals(win1)&&lucky==6){
-			System.out.println(test1);
-			System.out.println("èŠ±äº†"+2*count+"RMB");
-			break;
-		}else{
-			count++;
-		}
+			if(Arrays.equals(test,win)&&lucky==6){
+
+				System.out.println(Arrays.toString(test)+"["+lucky+"]");
+				System.out.println("¹²ÂòÁË"+count+"´Î"+"»¨ÁË"+2*count+"RMB");
+				break;
+			}else{
+				count++;
+			}
 
 
         }
